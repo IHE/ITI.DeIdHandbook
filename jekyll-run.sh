01e79ece5acbd6028ab5d38490da7278829febef
AlanZhang <zfchang_ai@icloud.comn>
2025-03-10 20:55:03 +0800

    Revert "created a customized IG publish script"
    This reverts commit 0ac0b1d433118cc0be46fcc804dd67d8087b3713.

--- jekyll-run.sh
+++ jekyll-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# get root dir of the shell env
+# 获取脚本所在目录（根目录）
 ROOT_DIR="$(dirname "$(realpath "$0")")"
 mkdir -p "$ROOT_DIR/temp/pages/_bibliography"
 
 # copy _bibliography
 cp "$ROOT_DIR/_bibliography/references.bib" "$ROOT_DIR/temp/pages/_bibliography/"
 
-# run Jekyll，specifi the configuration file and distination
+# 运行 Jekyll，指定配置文件和输出目录
 bundle exec jekyll build --config "$ROOT_DIR/_config.yml" --destination "$ROOT_DIR/output"
